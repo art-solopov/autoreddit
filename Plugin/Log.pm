544ccb2cb69b940507da558ae74c0881b65a1403
Artemiy Solopov <art-solopov@yandex.ru>
2013-08-30 00:37:21 +0400

    Added the first plugin and the very first test for it.

---
+++ Plugin/Log.pm
@@ -0,0 +1,64 @@
+=pod
+
+=head1 Plugin::Log
+
+A plugin that just logs the link instead of 
+downloading it
+
+=cut
+
+package Plugin::Log;
+
+=pod
+
+=head2 methods
+
+=head3 new
+
+The constructor. Accepts the log filename
+
+=cut
+
+sub new($$)
+{
+    my $cls = shift;
+    my $logfile = shift;
+    my $this = {logfile => $logfile};
+    return bless $this, $cls;
+}
+
+=pod
+
+=head3 is_processable
+
+Whether URL is processable by this plugin
+
+This plugin's version always returns true
+
+=cut
+
+sub is_acceptable($$)
+{
+    my $this = shift;
+    my $url = shift;
+    return true;
+}
+
+=pod
+
+=head3 process
+
+Processes the URL
+
+=cut
+
+sub process($$)
+{
+    my $this = shift;
+    my $url = shift;
+    open my $fh, '>>', $this->{ logfile };
+    print $fh "$url\n";
+    close $fh;
+}
+
+1;
---
+++ tests/plugin_load.t
@@ -0,0 +1,9 @@
+use Test::More;
+use lib '..';
+
+BEGIN 
+{
+    use_ok( 'Plugin::Log', 'Loading OK');
+}
+
+done_testing();
