80d83674d9d7fc780aa1f8a157cbe780ad177a93
Artemiy Solopov <art-solopov@yandex.ru>
2013-10-03 09:48:27 +0400

    A little interface fix and addition.

--- lib/autoreddit.pm
+++ lib/autoreddit.pm
@@ -21,28 +21,49 @@ use JSON;
 
 =head1 METHODS
 
-=head2 get_saved_links
+=head2 get_saved_links($url)
 
-Downloads the list of saved links and parses its JSON.
+Downloads the list of saved links from Reddit URL $url and parses its JSON.
+
+Returns the hash from parsed JSON.
 
 =cut
 
 sub get_saved_links
 {
     my $this = shift;
-    my $config = shift;
+    my $url = shift;
     my $ua = LWP::UserAgent->new;
-    my $req = HTTP::Request->new(GET => $config->{ saved_url });
+    my $req = HTTP::Request->new(GET => $url);
     my $res = $ua->request($req);
     if ($res->is_success)
     {
-        my $json = decode_json($res->content);
-        return $json;
+        my $reddit_children = decode_json($res->content);
+        return $reddit_children;
     }
     else
     {
         print STDERR $res->status_line, "\n";
     };
+}
+
+=pod
+
+=head2 process_saved_links($plugins)
+
+Processes hash of saved links (retrieved by get_saved_links method) with the
+plugins $plugins.
+
+$plugins - array reference of plugin names.
+
+=cut
+
+sub process_saved_links
+{
+	my $this = shift;
+	my $plugins = shift;
+	# TODO realise
+	return;
 }
 
 1;
